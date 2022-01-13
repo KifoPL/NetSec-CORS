7e2dc938c3402c0b66c6220f57cb0a9fbe1080cf
KifoPL <kifounaabi@gmail.com>
2022-01-13 16:41:48 +0100

    added sample console app and web app consuming api

---
+++ Sample App Consuming API/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Sample_App_Consuming_API;
+
+public class Program
+{
+	private static readonly HttpClient HttpClient = new();
+
+	public static async Task Main(string[] args)
+	{
+
+		string json = await HttpClient.GetStringAsync("https://localhost:7042/page/3");
+
+		Console.WriteLine(json);
+	}
+}
